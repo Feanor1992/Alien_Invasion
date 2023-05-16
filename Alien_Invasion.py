c7da926a73aefdae01948b8bcb365090ef71b337
Feanor1992 <feanor1992@gmail.com>
2023-05-16 11:06:07 +0300

    add main file Alien_Invasion.py with init class

---
+++ Alien_Invasion.py
@@ -0,0 +1,39 @@
+import sys
+import pygame
+import ctypes
+
+
+class AlienInvasion:
+    """class for resource management and game behavior"""
+    def __init__(self):
+        """init game and build game resources"""
+        pygame.init()
+
+        # get screen size from users settings
+        # user32 = ctypes.windll.user32
+        # screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
+        # self.screen = pygame.display.set_mode(screensize)
+        self.screen = pygame.display.set_mode((1280, 800))
+        pygame.display.set_caption('Alien Invasion')
+
+        # assigning a background colour
+        self.bg_color = (230, 230, 230)
+
+    def run_game(self):
+        """start of main loop"""
+        while True:
+            # catch clicks by mouse and keyboard
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            self.screen.fill(self.bg_color)
+
+            # displaying the last drawn screen
+            pygame.display.flip()
+
+
+if __name__ == '__main__':
+    # initiate and run the game
+    ai = AlienInvasion()
+    ai.run_game()
